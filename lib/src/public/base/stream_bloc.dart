419655048f2d3d600478d5c775d48453a5ff8df4
Yakov Karpov <yakovkarpoff@gmail.com>
2021-12-24 03:21:50 +0300

    Add documentation to StreamBloc

--- lib/src/public/base/stream_bloc.dart
+++ lib/src/public/base/stream_bloc.dart
@@ -3,6 +3,11 @@ import 'package:stream_bloc/src/private/mixins/stream_bloc_hooks_mixin.dart';
 import 'package:stream_bloc/src/private/mixins/stream_bloc_transformers_mixin.dart';
 import 'package:stream_bloc/src/public/interfaces/stream_bloc.dart';
 
+/// Transforms input `Events` using [mapEventToStates] into [Stream] of [States]
+/// as an output.
+///
+/// Almost identical to pre-`7.2.0` Bloc from the `bloc` package.
+
 abstract class StreamBloc<Event extends Object?,
         State extends Object?> = StreamBlocBase<Event, State>
     with
