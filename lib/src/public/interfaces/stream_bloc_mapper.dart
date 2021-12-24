01c02c0ad74c76001129bb5f1d705f863069b982
Yakov Karpov <yakovkarpoff@gmail.com>
2021-12-24 03:02:03 +0300

    Update head bloc dependency

--- lib/src/public/interfaces/stream_bloc_mapper.dart
+++ lib/src/public/interfaces/stream_bloc_mapper.dart
@@ -1,12 +1,6 @@
 import 'package:meta/meta.dart';
 
 abstract class StreamBlocMapper<Event extends Object?, State extends Object?> {
-  /// Whether the bloc is closed.
-  ///
-  /// A bloc is considered closed once [close] is called.
-  /// Subsequent state changes cannot occur within a closed bloc.
-  bool get isClosed;
-
   /// Must be implemented when a class extends [IStreamBloc].
   /// [mapEventToStates] is called whenever an [event] is [add]ed
   /// and is responsible for converting that [event] into a new [state].
