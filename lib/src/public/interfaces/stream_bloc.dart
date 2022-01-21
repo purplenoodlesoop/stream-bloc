0eaac42249a5ee1b5e91de0671ad634935cd0405
Dima <z-dima@live.ru>
2022-01-21 19:38:25 +0300

    StreamBlocStates public interface

--- lib/src/public/interfaces/stream_bloc.dart
+++ lib/src/public/interfaces/stream_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:stream_bloc/src/public/interfaces/stream_bloc_hooks.dart';
 import 'package:stream_bloc/src/public/interfaces/stream_bloc_mapper.dart';
+import 'package:stream_bloc/src/public/interfaces/stream_bloc_states.dart';
 import 'package:stream_bloc/src/public/interfaces/stream_bloc_transformers.dart';
 
 abstract class IStreamBloc<Event extends Object?, State extends Object?>
@@ -9,4 +10,5 @@ abstract class IStreamBloc<Event extends Object?, State extends Object?>
         BlocEventSink<Event>,
         StreamBlocMapper<Event, State>,
         StreamBlocTransformers<Event, State>,
-        StreamBlocHooks<Event, State> {}
+        StreamBlocHooks<Event, State>,
+        StreamBlocStates<Event, State> {}
--- lib/src/public/interfaces/stream_bloc_states.dart
+++ lib/src/public/interfaces/stream_bloc_states.dart
@@ -1,31 +1,22 @@
-import 'package:stream_bloc/src/private/base/stream_bloc_base.dart';
-import 'package:stream_bloc/src/public/interfaces/stream_bloc_states.dart';
-
-mixin StreamBlocStatesMixin<Event extends Object?, State extends Object?>
-    on StreamBlocBase<Event, State> implements StreamBlocStates<Event, State> {
+abstract class StreamBlocStates<Event extends Object?, State extends Object?> {
   /// This transformer is a shorthand for Stream.where followed by Stream.cast.
   ///
   /// [State]'s that do not match [T] are filtered out,
   ///  the resulting Stream will be of Type [T].
-  @override
-  Stream<T> whereState<T extends Object?>() =>
-      stream.where((state) => state is T).cast<T>();
+  Stream<T> whereState<T extends Object?>();
 
   /// Filter with whereState<T>() and after that
   /// skips [State]'s if they are equal to the previous data event.
   ///
   /// [State]'s that do not match [T] are filtered out,
   ///  the resulting Stream will be of Type [T].
-  @override
-  Stream<T> whereUnique<T extends Object?>() => whereState<T>().distinct();
+  Stream<T> whereUnique<T extends Object?>();
 
   /// This transformer leaves only the necessary states with downcast to [T]
   ///
   /// [State]'s that do not match [T] are filtered out,
   ///  the resulting Stream will be of Type [T].
-  @override
-  Stream<T> whereStates<T extends Object?>(bool Function(State state) filter) =>
-      stream.where(filter).where((state) => state is T).cast<T>();
+  Stream<T> whereStates<T extends Object?>(bool Function(State state) filter);
 
   /// Filter with whereStates<T>() and after that
   /// skips [State]'s if they are equal to the previous data event.
@@ -33,8 +24,5 @@ mixin StreamBlocStatesMixin<Event extends Object?, State extends Object?>
   ///
   /// [State]'s that do not match [T] are filtered out,
   ///  the resulting Stream will be of Type [T].
-  @override
-  Stream<T> whereUniques<T extends Object?>(
-          bool Function(State state) filter) =>
-      whereStates<T>(filter).distinct();
+  Stream<T> whereUniques<T extends Object?>(bool Function(State state) filter);
 }
