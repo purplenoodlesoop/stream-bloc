f1b01319d4da87c9a37af6205dd303829bef808e
Dima <z-dima@live.ru>
2022-01-25 10:04:48 +0300

    keeping only two methods – whereState and whereUnique

--- lib/src/private/mixins/stream_bloc_states_mixin.dart
+++ lib/src/private/mixins/stream_bloc_states_mixin.dart
@@ -18,23 +18,4 @@ mixin StreamBlocStatesMixin<Event extends Object?, State extends Object?>
   ///  the resulting Stream will be of Type [T].
   @override
   Stream<T> whereUnique<T extends Object?>() => whereState<T>().distinct();
-
-  /// This transformer leaves only the necessary states with downcast to [T]
-  ///
-  /// [State]'s that do not match [T] are filtered out,
-  ///  the resulting Stream will be of Type [T].
-  @override
-  Stream<T> whereStates<T extends Object?>(bool Function(State state) filter) =>
-      stream.where(filter).where((state) => state is T).cast<T>();
-
-  /// Filter with whereStates<T>() and after that
-  /// skips [State]'s if they are equal to the previous data event.
-  /// Downcast states to [T]
-  ///
-  /// [State]'s that do not match [T] are filtered out,
-  ///  the resulting Stream will be of Type [T].
-  @override
-  Stream<T> whereUniques<T extends Object?>(
-          bool Function(State state) filter) =>
-      whereStates<T>(filter).distinct();
 }
--- lib/src/public/interfaces/stream_bloc_states.dart
+++ lib/src/public/interfaces/stream_bloc_states.dart
@@ -11,18 +11,4 @@ abstract class StreamBlocStates<Event extends Object?, State extends Object?> {
   /// [State]'s that do not match [T] are filtered out,
   ///  the resulting Stream will be of Type [T].
   Stream<T> whereUnique<T extends Object?>();
-
-  /// This transformer leaves only the necessary states with downcast to [T]
-  ///
-  /// [State]'s that do not match [T] are filtered out,
-  ///  the resulting Stream will be of Type [T].
-  Stream<T> whereStates<T extends Object?>(bool Function(State state) filter);
-
-  /// Filter with whereStates<T>() and after that
-  /// skips [State]'s if they are equal to the previous data event.
-  /// Downcast states to [T]
-  ///
-  /// [State]'s that do not match [T] are filtered out,
-  ///  the resulting Stream will be of Type [T].
-  Stream<T> whereUniques<T extends Object?>(bool Function(State state) filter);
 }
