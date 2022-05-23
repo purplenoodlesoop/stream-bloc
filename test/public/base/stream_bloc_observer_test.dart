708520e639f00344931b69e81805b90ed3bc8fe8
Egor Popov <nxtswitch@gmail.com>
2022-05-23 10:54:37 +0300

    Test for correctness of the StreamBlocObserver default tag

---
+++ test/public/base/stream_bloc_observer_test.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:stream_bloc/stream_bloc.dart';
+import 'package:test/test.dart';
+
+@visibleForTesting
+class TestStreamBlocObserver extends StreamBlocObserver {}
+
+void main() {
+  group('StreamBlocObserver', () {
+    test(
+      'should return same observer instance when override [zoneValues]',
+      () async {
+        // arrange
+        final observer = TestStreamBlocObserver();
+
+        // act
+        final result = StreamBlocObserver.inject(
+          observer,
+          () => runZoned(
+            () => StreamBlocObserver.current,
+            zoneValues: {const Object(): TestStreamBlocObserver()},
+          ),
+        );
+
+        // assert
+        expect(result, same(observer));
+      },
+    );
+  });
+}
